2d6671b9c7fd89be70b0b94a9779e87dcaec6dab
KingBonfante <72134861+KingBonfante@users.noreply.github.com>
2024-04-17 00:54:28 +0000

    feat: courses e footer

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,15 @@
 import Header from "./components/Header";
 import Banner from "./components/Banner";
+import Courses from "./components/Courses";
+import Footer from "./components/Footer";
 
 export default function Home() {
   return (
    <div>
       <Header/>
       <Banner/>
+      <Courses/>
+      <Footer/>
    </div>
   );
 }
